a3493e3a505787c6f93b98c5013931286f682cab
nmrastogi <nmrastogi@gmail.com>
2026-01-01 16:48:14 -0800

    Refactor producer and consumer functions for improved readability and consistency. Simplify job production loop and enhance formatting in the main function. Ensure proper channel closure and synchronization with WaitGroup.

--- producer_consumer.go
+++ producer_consumer.go
@@ -7,37 +7,37 @@ import (
 )
 
 func producer(id int, jobs chan<- int) {
-	for i := 0; i<10; i++{
-		job :=id*100+i;
-		fmt.Printf("producer %d: produced %d\n", id, job);
-		jobs <- job;
-		time.Sleep(time.Millisecond * 100);
+	for i := 0; i < 5; i++ {
+		job := id*100 + i
+		fmt.Printf("producer %d: produced %d\n", id, job)
+		jobs <- job
+		time.Sleep(time.Millisecond * 100)
 	}
 }
 
 func consumer(id int, jobs <-chan int, wg *sync.WaitGroup) {
-	defer wg.Done();
-	for job := range jobs{
-		fmt.Printf("consumer %d: consumed %d\n", id, job);
-		time.Sleep(time.Millisecond * 200);
+	defer wg.Done()
+	for job := range jobs {
+		fmt.Printf("consumer %d: consumed %d\n", id, job)
+		time.Sleep(time.Millisecond * 200)
 	}
 }
 
 func main() {
-	jobs := make(chan int, 100);
-	var wg sync.WaitGroup;
+	jobs := make(chan int, 100)
+	var wg sync.WaitGroup
 
-	for i :=1; i<=2; i++{
-		wg.Add(1);
-		go consumer(i, jobs, &wg);
+	for i := 1; i <= 2; i++ {
+		wg.Add(1)
+		go consumer(i, jobs, &wg)
 	}
 
-	for i :=1; i<=2; i++{
-		go producer(i,jobs);
+	for i := 1; i <= 2; i++ {
+		go producer(i, jobs)
 	}
 
-	time.Sleep(time.Second * 2);
-	close(jobs);
-	wg.Wait();
+	time.Sleep(time.Second * 2)
+	close(jobs)
+	wg.Wait()
 
 }
